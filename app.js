20c4b3f4fd3b81cbd281cfdfac681a40cad34289
Khakim Zhumagaliyev <cyber@Khakims-MacBook-Air.local>
2021-03-11 16:23:57 +0600

    dynamic changes

--- app.js
+++ app.js
@@ -1,40 +1,66 @@
-let slideDots = document.querySelectorAll('.slider-items .item')
-let placeCity = document.querySelectorAll('.place-city')
-let apartImages = document.querySelectorAll('.apart-img')
-let cityInfo = document.querySelectorAll('.info-container')
-let imageIndex = 1;
-show_apart(imageIndex)
-
-function nextSlide(){
-  if(imageIndex > apartImages.length){
-    imageIndex = 1
+const images = [
+  {
+    img : 'image1',
+    alt : 'place1',
+    titleContent : 'Rostov-on-Don LCD admiral',
+    areaAps : '81 m2',
+    repairTime : '3.5 months',
+    repairCost : 'Upon Request'
+  },
+  {
+    img:  'image2',
+    alt : 'place2',
+    titleContent : 'Sochi Thieves',
+    areaAps : '105 m2',
+    repairTime : '4 months',
+    repairCost : 'Upon Request'
+  },
+  {
+    img : 'image3',
+    alt : 'place3',
+    titleContent : 'Rostov-on-Don Patriotic',
+    areaAps : '93 m2',
+    repairTime : '3 months',
+    repairCost : 'Upon Request'
   }
-  show_apart(imageIndex += 1)
-}
-function previousSlide(){
-  if(imageIndex < 1){
-    imageIndex = apartImages.length
-  }
-  show_apart(imageIndex -= 1)
-}
-function currentSlide(i){
-  show_apart(imageIndex = i)
-}
-
-function show_apart(imageIndex){
-  if(imageIndex > apartImages.length){
-    imageIndex = 1
-  }else if(imageIndex < 1){
-    imageIndex = apartImages.length
-  }
-  for (let i = 0; i < apartImages.length; i++) {
-    apartImages[i].style.display = "none"
-    cityInfo[i].classList.remove('active')
+]
+const slideDots = document.querySelectorAll('.slider-items .item')
+const imagesContainer = document.getElementById('uniqueApart')
+const catalogueImg = document.getElementById('catalogueImg')
+const placeCity = document.querySelectorAll('.place-city')
+const titleCont = document.querySelector('.title_city')
+const areaApart = document.querySelector('.title_area')
+const repairTim = document.querySelector('.title_repair')
+const repairCos = document.querySelector('.title_cost')
+let currentIndex = 0
+const setEntity = (index) => {
+  for(let i = 0; i < images.length; i++){
     slideDots[i].classList.remove('active')
     placeCity[i].classList.remove('active')
-  } 
-  apartImages[imageIndex - 1].style.display = "block"
-  cityInfo[imageIndex - 1].classList.add('active')
-  slideDots[imageIndex - 1].classList.add('active')
-  placeCity[imageIndex - 1].classList.add('active')
+  }
+  catalogueImg.src = `images/${images[index].img}.png`
+  catalogueImg.alt = `${images[index].alt}`
+  slideDots[index].classList.add('active')
+  placeCity[index].classList.add('active')
+  titleCont.textContent = images[index].titleContent
+  areaApart.textContent = images[index].areaAps
+  repairTim.textContent = images[index].repairTime
+  repairCos.textContent = images[index].repairCost
 }
+setEntity(currentIndex)
+function nextSlide(){
+  currentIndex++
+  currentIndex %= images.length
+  setEntity(currentIndex)
+}
+function previousSlide(){
+  if(currentIndex < 1){
+    currentIndex = images.length
+  }
+  setEntity(currentIndex - 1)
+  currentIndex -= 1
+}
+function currentSlide(i){
+  setEntity(currentIndex = i)
+}
+
